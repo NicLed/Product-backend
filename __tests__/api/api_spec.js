991016da0f4cdf57a5e5317472d6e0f376866930
Nicholas Ledermann <nic.ledermann@gmail.com>
2022-03-09 11:26:31 -0600

    Update package.json and .gitignore

--- __tests__/api/api_spec.js
+++ __tests__/api/api_spec.js
@@ -7,28 +7,28 @@ const frisby = require('frisby');
 
 it ('GET /products should return a status of 200 OK', function () {
   return frisby
-    .get('http://localhost:3030/products')
+    .get('http://ec2-3-83-49-124.compute-1.amazonaws.com:3030/products')
     .expect('status', 200)
     // .inspectJSON();
 });
 
 it ('GET /products/:product_id should return a status of 200 OK', function () {
   return frisby
-    .get('http://localhost:3030/products/1')
+    .get('http://ec2-3-83-49-124.compute-1.amazonaws.com:3030/products/1')
     .expect('status', 200)
     // .inspectJSON();
 });
 
 it ('GET /products/:product_id/styles should return a status of 200 OK', function () {
   return frisby
-    .get('http://localhost:3030/products/1/styles')
+    .get('http://ec2-3-83-49-124.compute-1.amazonaws.com:3030/products/1/styles')
     .expect('status', 200)
     // .inspectJSON();
 });
 
 it ('GET /products/:product_id/related should return a status of 200 OK', function () {
   return frisby
-    .get('http://localhost:3030/products/1/related')
+    .get('http://ec2-3-83-49-124.compute-1.amazonaws.com:3030/products/1/related')
     .expect('status', 200)
     .inspectJSON();
 });
